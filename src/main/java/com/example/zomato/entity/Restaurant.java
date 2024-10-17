202998e0e66f6fe40ec6e3e9f54e6ea761b72017
pavankumar0809 <basavarajunn18@gmail.com>
2024-10-17 15:03:24 +0530

    Created RestaturantRepository interface

--- src/main/java/com/example/zomato/entity/Restaurant.java
+++ src/main/java/com/example/zomato/entity/Restaurant.java
@@ -2,22 +2,32 @@ package com.example.zomato.entity;
 
 import com.example.zomato.config.GenerateCustomId;
 import com.example.zomato.enums.DietType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "restaurants")
+@Getter
+@Setter
 public class Restaurant {
+
     @Id
     @GenerateCustomId
+    @Column(name = "restaurantId")
     private String restaurantId;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "phoneNumber")
     private String phoneNumber;
+    @Column(name = "email")
     private String email;
+    @Column(name = "diet_types")
+    @ElementCollection
     private List<DietType> dietTypes;
 }
 
