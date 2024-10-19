1ddcb7350ad468550ca38617de0d924b15e430fd
pavankumar0809 <basavarajunn18@gmail.com>
2024-10-19 15:42:28 +0530

    Mapped Address to restaurant

--- src/main/java/com/example/zomato/entity/Restaurant.java
+++ src/main/java/com/example/zomato/entity/Restaurant.java
@@ -29,5 +29,8 @@ public class Restaurant {
     @Column(name = "diet_types")
     @ElementCollection
     private List<DietType> dietTypes;
+    @Column(name = "address")
+    @OneToOne
+    private Address address;
 }
 
