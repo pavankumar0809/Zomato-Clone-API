a092daa2c0c2a033c24db3b000731d1df6db13c6
pavankumar0809 <basavarajunn18@gmail.com>
2024-10-19 15:50:20 +0530

    Created AddressRepository,  AddressController and AddressService

---
+++ src/main/java/com/example/zomato/controller/AddressController.java
@@ -0,0 +1,11 @@
+package com.example.zomato.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${zomato.base_url}")
+public class AddressController {
+
+}
