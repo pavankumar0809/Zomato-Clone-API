1f2c4b26d547a5d2f28d752d36b8d3580a0957d5
pavankumar0809 <basavarajunn18@gmail.com>
2024-10-19 15:39:40 +0530

    Created DTOs AddressRequest and AddressResponse

--- src/main/java/com/example/zomato/requestdtos/AddressRequest.java
+++ src/main/java/com/example/zomato/requestdtos/AddressRequest.java
@@ -1,5 +1,19 @@
 package com.example.zomato.requestdtos;
 
-public class AddressResponse {
-    private String
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddressRequest {
+
+    private String addressLine1;
+    private String addressLine2;
+    private String landmark;
+    private String area;
+    private String city;
+    private String state;
+    private String pincode;
+    private double latitude;
+    private double longitude;
 }
