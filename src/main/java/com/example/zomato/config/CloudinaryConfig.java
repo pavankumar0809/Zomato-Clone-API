425fcb3cdc8ea183f8e9ef10897d0ebda97e3e94
pavankumar0809 <basavarajunn18@gmail.com>
2024-10-19 21:52:49 +0530

    Created CloudinaryConfig and used @Value Annotation to initilalize fields

--- src/main/java/com/example/zomato/config/CloudinaryConfig.java
+++ src/main/java/com/example/zomato/config/CloudinaryConfig.java
@@ -2,15 +2,19 @@ package com.example.zomato.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${cloudinary.cloud_name}")
     private String cloudName;
 
+    @Value("${cloudinary.cloud_apikey}")
     private String apikey;
 
+    @Value("${cloudinary.cloud_api_secret}")
     private String apiSecret;
 
     @Bean
