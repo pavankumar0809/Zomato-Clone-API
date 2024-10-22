349e9f5606b3a29bed5f3f7eb03bef7bcfca6477
pavankumar0809 <basavarajunn18@gmail.com>
2024-10-22 16:58:01 +0530

    Genereated DTOs for Cuisine Entity

---
+++ src/main/java/com/example/zomato/responsedtos/CuisineResponse.java
@@ -0,0 +1,11 @@
+package com.example.zomato.responsedtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CuisineResponse {
+    private String cuisineId;
+    private String title;
+}
