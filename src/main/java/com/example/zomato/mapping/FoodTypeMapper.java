396f774607a7c9d954a9363a404da13666950909
pavankumar0809 <basavarajunn18@gmail.com>
2024-10-24 15:46:54 +0530

    Created FoodType Entity, DTOs for FoodType and generated Mapper class for FoodType class

---
+++ src/main/java/com/example/zomato/mapping/FoodTypeMapper.java
@@ -0,0 +1,19 @@
+package com.example.zomato.mapping;
+
+import com.example.zomato.entity.FoodType;
+import com.example.zomato.requestdtos.FoodTypeRequest;
+import com.example.zomato.responsedtos.FoodTypeResponse;
+
+public class FoodTypeMapper {
+    public FoodType mapToFoodType(FoodTypeRequest foodTypeRequest, FoodType foodType) {
+        foodType.setTitle(foodTypeRequest.getTitle());
+        return foodType;
+    }
+
+    public FoodTypeResponse mapToFoodTypeResponse(FoodType foodType) {
+        FoodTypeResponse foodTypeResponse = new FoodTypeResponse();
+        foodTypeResponse.setTypeId(foodType.getTypeId());
+        foodTypeResponse.setTitle(foodType.getTitle());
+        return foodTypeResponse;
+    }
+}
