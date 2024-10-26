620546809056ee1439fa6d75683baf452ca9afdd
pavankumar0809 <basavarajunn18@gmail.com>
2024-10-26 17:26:50 +0530

    Implimenting PasswordEncoder and AuthenticationProvider interfaces

--- src/main/java/com/example/zomato/repository/UserRepository.java
+++ src/main/java/com/example/zomato/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.zomato.repository;
 import com.example.zomato.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+     Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/com/example/zomato/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.zomato.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.AuthProvider;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+}
